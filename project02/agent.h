50df32cce504a7d4f01906a803f40b0634df3918
Thong The Nguyen <nguyenthethong1996@gmail.com>
2019-11-06 23:00:28 +0800

    change learning rate

--- project02/agent.h
+++ project02/agent.h
@@ -245,7 +245,7 @@ private:
     std::vector<State> ep;
 
 
-    float learning_rate = 0.025;
+    float learning_rate = 0.00025;
     int tuple_index[4][6] = {
             { 0,  1,  2,  3,  4,  5},
             { 4,  5,  6,  7,  8,  9},
