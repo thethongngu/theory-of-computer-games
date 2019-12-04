6e2e7e4d5b6947c3ec9f64c904bf455713bef6bb
Thong The Nguyen <nguyenthethong1996@gmail.com>
2019-12-04 22:57:21 +0800

    add merging regions

--- project04/main.cpp
+++ project04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 #define ull unsigned long long
 #define ui  unsigned int
@@ -53,15 +54,44 @@ struct Board {
     ui second_seg;
     ull first_flag;
     ui second_flag;
+
+    std::vector<Region> regions;
+    int cell_to_region[NUM_CELL];
 };
 
 void init_board(Board &board) {
     board.first_seg = board.first_flag = 0;
     board.second_seg = board.second_flag = 0;
+    board.regions.clear();
+}
+
+void init_region(Region &region) {
+    region.first_seg = region.first_flag = 0;
+    region.second_seg = region.second_flag = 0;
+}
+
+void get_adj_cells(int pos, std::vector<int> &res) {
+    assert(pos >= 0 && pos <= NUM_CELL - 1);
+
+    int d[4] = {-BOARD_SIZE, +1, +BOARD_SIZE, -1};
+
+    for (int i = 0; i < 4; i++) {
+        int new_pos = pos + d[i];
+        if (new_pos >= 0 && new_pos <= NUM_CELL - 1) res.push_back(new_pos);
+    }
+}
+
+int get_region_id_by_cell(const Board &tmp, int pos) {
+    return tmp.cell_to_region[pos];
+}
+
+bool compare_region(Region ra, Region rb) {
+    return ra.first_flag == rb.first_flag && ra.second_flag == rb.second_flag &&
+           ra.first_seg == rb.first_seg && ra.second_seg == rb.second_seg;
 }
 
 void on_bit_region(Region &region, int i) {
-    assert(i >= 0 && i <= 80);
+    assert(i >= 0 && i <= NUM_CELL - 1);
     if (i < 64) {
         region.first_seg |= (1 << i);
         region.first_flag |= (1 << i);
@@ -72,7 +102,7 @@ void on_bit_region(Region &region, int i) {
 }
 
 void off_bit_region(Region &region, int i) {
-    assert(i >= 0 && i <= 80);
+    assert(i >= 0 && i <= NUM_CELL - 1);
     if (i < 64) {
         region.first_seg &= ~(1 << i);
         region.first_flag &= ~(1 << i);
@@ -82,8 +112,19 @@ void off_bit_region(Region &region, int i) {
     }
 }
 
-void on_bit_board(Board &board, int i) {
-    assert(i >= 0 && i <= 80);
+void add_black_to_bit(Board &board, int i) {
+    assert(i >= 0 && i <= NUM_CELL - 1);
+    if (i < 64) {
+        board.first_seg &= ~(1 << i);
+        board.first_flag |= (1 << i);
+    } else {
+        board.second_seg &= ~(1 << i);
+        board.second_flag |= (1 << i);
+    }
+}
+
+void add_white_to_bit(Board &board, int i) {
+    assert(i >= 0 && i <= NUM_CELL - 1);
     if (i < 64) {
         board.first_seg |= (1 << i);
         board.first_flag |= (1 << i);
@@ -93,8 +134,8 @@ void on_bit_board(Board &board, int i) {
     }
 }
 
-void off_bit_board(Board &board, int i) {
-    assert(i >= 0 && i <= 80);
+void remove_cell_on_board(Board &board, int i) {
+    assert(i >= 0 && i <= NUM_CELL - 1);
     if (i < 64) {
         board.first_seg &= ~(1 << i);
         board.first_flag &= ~(1 << i);
@@ -104,8 +145,26 @@ void off_bit_board(Board &board, int i) {
     }
 }
 
+int get_region_cell(const Region &region, int pos) {
+    assert(pos >= 0 && pos <= NUM_CELL - 1);
+    if (pos < 64) {
+        int is_empty = ~((region.first_flag & (1 << pos)) >> pos);
+        if (is_empty) return NONE;
+
+        int bit = (region.first_seg & (1 << pos)) >> pos;
+        return (bit) ? WHITE : BLACK;
+
+    } else {
+        int is_empty = ~((region.second_flag & (1 << pos)) >> pos);
+        if (is_empty) return NONE;
+
+        int bit = (region.second_seg & (1 << pos)) >> pos;
+        return (bit) ? WHITE : BLACK;
+    }
+}
+
 int get_board_cell(const Board &board, int pos) {
-    assert(pos >= 0 && pos <= 80);
+    assert(pos >= 0 && pos <= NUM_CELL - 1);
     if (pos < 64) {
         int is_empty = ~((board.first_flag & (1 << pos)) >> pos);
         if (is_empty) return NONE;
@@ -122,6 +181,33 @@ int get_board_cell(const Board &board, int pos) {
     }
 }
 
+void merge_region(int fr_id, int sr_id, Board &board) {
+    // one cell can not in 2 different regions
+    Region &fr = board.regions[fr_id];
+    Region &sr = board.regions[sr_id];
+
+    assert((fr.first_flag & sr.first_flag) != 0);
+    assert((fr.second_flag & sr.second_flag) != 0);
+
+    // merging region
+    fr.first_seg |= sr.first_seg;
+    fr.second_seg |= sr.second_seg;
+    fr.first_flag |= sr.first_flag;
+    fr.second_flag |= sr.second_flag;
+
+    // update mapping cell to region
+    // for each cell in second region, change mapping to first region
+    for(int i = 0; i < 81; i++) {
+        int sr_color = get_region_cell(sr, i);
+        // TODO: how to assert color of two region similar?
+        if (sr_color == NONE) continue;
+        board.cell_to_region[i] = fr_id;
+    }
+
+    // remove the second regions out of board state
+    board.regions.erase(board.regions.begin() + sr_id);
+}
+
 /**
  * Set a cell of board require:
  *  - Empty cell
@@ -133,14 +219,28 @@ int get_board_cell(const Board &board, int pos) {
  *  - 2: fail, board not change
  */
 int set_board_cell(Board &board, int pos, int color) {
-    assert(pos >=0 && pos <= 80);
-    assert(color >=0 && color <= 2);
+    assert(pos >= 0 && pos <= NUM_CELL - 1);
+    assert(color == BLACK || color == WHITE);
 
     if (get_board_cell(board, pos) != NONE) return -1;
 
-    Board tmp_board = board;  // copy value
-    if (color == BLACK) off_bit_board(board, pos);
-    else on_bit_board(board, pos);
+    Board tmp = board;  // copy value
+    if (color == BLACK) add_black_to_bit(tmp, pos);
+    else add_white_to_bit(tmp, pos);
+
+    // update region information
+    std::vector<int> adjs;
+    get_adj_cells(pos, adjs);
+    int pos_region_id = get_region_id_by_cell(tmp, pos);
+
+    // check whether regions in 4 direction are the same
+    for (int &adj : adjs) {
+        if (get_board_cell(tmp, adj) != color) continue;
+        int adj_region_id = get_region_id_by_cell(tmp, adj);
+        if (pos_region_id == adj_region_id) continue;
+        merge_region(pos_region_id, adj_region_id, tmp);
+    }
+
 
 }
 
