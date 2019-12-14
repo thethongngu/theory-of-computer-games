7e21612f9086b1867aae6c5b5eebad9654232a51
Thong The Nguyen <nguyenthethong1996@gmail.com>
2019-12-15 02:20:59 +0800

    add get_best_child

--- project04/global.h
+++ project04/global.h
@@ -12,3 +12,6 @@
 #define ull unsigned long long
 #define BLACK 0
 #define WHITE 1
+
+#define C_BIAS 0.25
+#define SQR_B 0.01
--- project04/node.h
+++ project04/node.h
@@ -8,6 +8,9 @@
 #endif //PROJECT04_NODE_H
 
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 #include "global.h"
 
 class Node {
@@ -16,7 +19,40 @@ public:
     int last_pos;
     int last_color;
 
+    int win, count;
+    int rave_win, rave_count;
+
     Node* parent;
 
     std::vector<Node*> children;
+
+    double get_score() {
+        double ucb = (double)win / count + C_BIAS * sqrt(log(parent->count) / count);
+        double rave = (double)rave_win / rave_count;
+        double beta = (double)rave_count / (count + rave_count + 4 * count * rave_count * SQR_B);
+        return (1 - beta) * ucb + beta * rave;
+    }
+
+    Node* get_best_child() {
+        if (children.empty()) return nullptr;
+
+        std::vector<Node*> chosen;
+        double curr_score = children[0]->get_score();
+        chosen.push_back(children[0]);
+
+        for(Node* child: children) {
+            double score = child->get_score();
+            if (score - curr_score > -0.0001) {  // score >= curr_score
+                if (score - curr_score > 0.0001) {  // score > curr_score
+                    chosen.clear();
+                    chosen.push_back(child);
+                } else {
+                    chosen.push_back(child);
+                }
+            }
+        }
+
+        srand(time(NULL));
+        return chosen[std::rand() % chosen.size()];
+    }
 };
