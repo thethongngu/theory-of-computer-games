3ca45031956d979cb36bbb712700682da25b9071
thethongngu <nguyenthethong1996@gmail.com>
2020-01-11 02:13:36 +0800

    fix node

--- project05/node.cpp
+++ project05/node.cpp
@@ -1,70 +1,67 @@
-//
-// Created by thethongngu on 2020-01-10.
-//
-
 #include <cmath>
 #include <cstring>
-#include "global.h"
+#include <iostream>
 #include "node.h"
 
+using namespace std;
+
+Node::Node() {}
+
+Node::~Node() {
+    if (child_ptr != nullptr) {
+        delete[] child_ptr;
+        child_ptr = nullptr;
+    }
+}
+
+
 void Node::init_node(int pos, int color) {
-    last_pos = pos;
     last_color = color;
-    mean = 0.5;  count = 0;
-    rave_mean = 0.5;  rave_count = 20;
+    last_pos = pos;
+    mean = 0.5;
+    count = 0;
+    rave_count = 20;
+    rave_mean = 0.5;
+    num_child = 0;
     memset(child_pos, -1, sizeof(child_pos));
     child_ptr = nullptr;
     log_count = 1;
 }
 
-void Node::add_normal_result(double outcome) {
+void Node::add_normal_result(double result) {
     double value = 0.0;
-    if (outcome > 0 && last_color == BLACK) value = 1.0;
-    if (outcome < 0 && last_color == WHITE) value = 1.0;
+    if (result > 0 && last_color == BLACK) value = 1.0;
+    if (result < 0 && last_color == WHITE) value = 1.0;
+
     mean = (mean * count + value) / (count + 1);
-    count += 1;  log_count = log(count);
+    count += 1;
+    log_count = log(count);
 }
 
-void Node::add_rave_result(double outcome) {
+void Node::add_rave_result(double result) {
     double value = 0.0;
-    if (outcome > 0 && last_color == BLACK) value = 1.0;
-    if (outcome < 0 && last_color == WHITE) value = 1.0;
+    if (result > 0 && last_color == BLACK) value = 1.0;
+    if (result < 0 && last_color == WHITE) value = 1.0;
+
     rave_mean = (rave_mean * rave_count + value) / (rave_count + 1);
     rave_count += 1;
 }
 
-int Node::get_max_move() {
+void Node::expansion(Board &b) {
 
-    int max_count = -1, curr_count;
-    int child_id;
-
-    for (int i = 0; i < num_child; i++) {
-        curr_count = child_ptr[i].count;
-        if (max_count < curr_count) {
-            max_count = curr_count;
-            child_id = i;
-        }
-    }
-
-    return child_id;
-}
-
-void Node::expansion(Board &board) {
-
-    int pos;
-    int color = Board::change_color(last_color);
+    int pos, color;
+    color = !b.just_play_color();
     num_child = 0;
-
     for (pos = 0; pos < NUM_CELL; pos++) {
-        if (board.can_move(pos, color)) num_child++;
+        if (b.can_move(pos, color)) num_child++;
     }
-
     if (num_child == 0) return;
 
     child_ptr = new Node[num_child];
+
     int child_id = 0;
-    for(pos=0; pos < NUM_CELL; pos++) {
-        if(board.can_move(pos, color)) {
+    for (pos = 0; pos < NUM_CELL; pos++) {
+        if (b.can_move(pos, color)) {
             child_pos[pos] = child_id;
             child_ptr[child_id].init_node(pos, color);
             child_id++;
@@ -72,12 +69,59 @@ void Node::expansion(Board &board) {
     }
 }
 
+int Node::get_max_move() {
 
-Node::Node() {}
-Node::~Node() {
-    if (child_ptr != nullptr) {
-        delete [] child_ptr;
-        child_ptr= nullptr;
+    int best_child = -1;
+    double max_count = -1, curr_count;
+
+    for (int i = 0; i < num_child; i++) {
+        curr_count = child_ptr[i].count;
+        if (curr_count > max_count) {
+            max_count = curr_count;
+            best_child = i;
+        }
+    }
+
+    return best_child;
+}
+
+vector<float> Node::getPolicy() {
+    vector<float> ret(NUM_CELL, 0);
+    float sum = 0;
+    for (int i = 0; i < num_child; i++) {
+        sum += child_ptr[i].count;
+    }
+    for (int i = 0; i < num_child; i++) {
+        ret[child_ptr[i].last_pos] = child_ptr[i].count / sum;
+    }
+    return ret;
+}
+
+void Node::show_child() {
+    for (int i = 0; i < num_child; i++) {
+        if (child_ptr[i].count < 10) continue;
+        cerr << inttoGTPstring((int) child_ptr[i].last_pos) << ' ' << child_ptr[i].mean << ' ' << child_ptr[i].count
+             << ' ';
+        //cerr<<sqrt( logc / ((childptr[i].count) - minusnum))* UCB_WEIGHT<<' ' ;
+        cerr << (child_ptr[i].rave_mean) << ' ' << (child_ptr[i].rave_count) << endl;
     }
 }
 
+float Node::show_inf(int best) {
+    cerr << "total : " << count - basenum << endl << "winrate :" << (child_ptr[best].mean);
+    //cerr<<"\nlog : "<<sqrt( logc / ((childptr[best].count) - minusnum))* UCB_WEIGHT ;
+    cerr << "\nrave : " << (child_ptr[best].rave_mean) << ' ' << (child_ptr[best].rave_count) << endl;
+    return child_ptr[best].mean;
+}
+
+string Node::inttoGTPstring(int i) {
+    string s = "A1";
+    s[0] += i / 9;
+    if (s[0] >= 'I')s[0]++;
+    s[1] += i % 9;
+    return s;
+}
+
+string Node::inttostring(int i) {
+    return inttoGTPstring(i);
+}
--- project05/node.h
+++ project05/node.h
@@ -7,28 +7,44 @@
 
 #endif //PROJECT05_NODE_H
 
+#include "global.h"
 #include "board.h"
+
+#define basenum 0
 
 class Node {
 
 public:
-
-    int last_pos;
-    int last_color;
-    int child_pos[NUM_CELL];
+    int last_pos, last_color;
+    int child_pos[NUM_CELL + 1];
     int num_child;
     Node *child_ptr;
 
-    double count, mean, rave_count, rave_mean;
+    double count, mean;
+    double rave_count, rave_mean;
     double log_count;
 
     void init_node(int pos, int color);
-    void add_normal_result(double outcome);
-    void add_rave_result(double outcome);
+
+    void add_normal_result(double result);
+
+    void add_rave_result(double result);
+
     int get_max_move();
 
-    void expansion(Board &board);
+    vector<float> getPolicy();
+
+    void show_child();
+
+    float show_inf(int child);
+
+    void expansion(Board &b);
+
+    string inttostring(int i);
+
+    string inttoGTPstring(int i);
 
     Node();
+
     ~Node();
 };
